b30bf7e4bcb84e351e2756d80822e1d52d2cd6a6
mgreen1092 <morgan.green0209@gmail.com>
2022-11-11 14:29:51 -0500

    can retrive main data of symbol search but struggling to access data within because labeled as 1. symbol

--- src/Components/Container/Container.js
+++ src/Components/Container/Container.js
@@ -6,21 +6,23 @@ import SymbolSearch from "./SymbolSearch/SymbolSearch";
 
 function Container () {
     const [input, setInput] = useState('')
-    // function getData () {
-    //  axios.get(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=appl&apikey=L9CIXKF2CPVF19PV`).then((response) => {
-    //     console.log(response)
-    //  })
-    // } 
     function handleChange (e) {
+        //obtains text input value of SymbolSearch
         setInput(e.target.value)
     }
     function handleSubmit () {
+        //Obtains symbol and company name based on the input from SymbolSearch
         let inputKey = input.toLowerCase()
         axios.get(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${inputKey}&apikey=L9CIXKF2CPVF19PV`).then((response) => {
-            console.log(response)
+            console.log(response.data.bestMatches)
+            let bestMatches = response.data.bestMatches
+            console.log(bestMatches, 'best matches')
+            //let displayBestMatches = bestMatches.map((bestMatch)=> {
+                //symbol: bestMatch.1. symbol
+            //})
         })
     }
-    console.log(input)
+    //console.log(input)
     return (
         <div>
             <SymbolSearch handleSubmit={handleSubmit} handleChange={handleChange} input={input} setInput={setInput}/>
