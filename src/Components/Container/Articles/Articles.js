e10a777b722804680c1bd46e6978e02d0ec62514
mgreen1092 <morgan.green0209@gmail.com>
2022-11-15 14:22:40 -0500

    styling to article text

--- src/Components/Container/Articles/Articles.js
+++ src/Components/Container/Articles/Articles.js
@@ -9,17 +9,19 @@ function Articles (props) {
                     return (
                         <div className='Article-section'>
                             <div className='Container'>
+                                <div className='Article-content'>
                                 <div className='Article-title'>
                                     <p><a href={article.url}>{article.title}</a></p>
                                 </div>
                                 <div className='Article-author'>
-                                    <p>Author: {article.authors[0]}</p>
+                                    <p><span id='Font'>Author:</span> {article.authors[0]}</p>
                                 </div>
                                 <div className='Article-date'>
-                                    <p>Date: {article.time_published}</p>
+                                    <p><span id='Font'>Date:</span> {article.time_published}</p>
                                 </div>
                                 <div className='Article-summary'>
-                                    <p>Summary: {article.summary}</p>
+                                    <p><span id='Font'>Summary:</span> {article.summary}</p>
+                                </div>
                                 </div>
                             </div>
                         </div>
