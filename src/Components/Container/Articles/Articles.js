25cbb97f4f0c43707e17041bb36f7cb83fac5068
mgreen1092 <morgan.green0209@gmail.com>
2022-11-15 14:10:01 -0500

    article styling

--- src/Components/Container/Articles/Articles.js
+++ src/Components/Container/Articles/Articles.js
@@ -3,16 +3,26 @@ import './Articles.css'
 
 function Articles (props) {
     return (
-        <div className='Article-section'>
-            <h2 className='Article-title'>News Feed:</h2>
+        <div>
+            <h2 className='News-feed'>News Feed:</h2>
                 {props.articles?.map((article) => {
                     return (
-                        <container className='Container'>
-                            <div>Title: <a href={article.url}>{article.title}</a></div>
-                            <div>Author: {article.authors[0]}</div>
-                            <div>Date: {article.time_published}</div>
-                            <div>Summary: {article.summary}</div>
-                        </container>
+                        <div className='Article-section'>
+                            <div className='Container'>
+                                <div className='Article-title'>
+                                    <p><a href={article.url}>{article.title}</a></p>
+                                </div>
+                                <div className='Article-author'>
+                                    <p>Author: {article.authors[0]}</p>
+                                </div>
+                                <div className='Article-date'>
+                                    <p>Date: {article.time_published}</p>
+                                </div>
+                                <div className='Article-summary'>
+                                    <p>Summary: {article.summary}</p>
+                                </div>
+                            </div>
+                        </div>
                     )
                 })}
         </div>
