ed5943963168ef85ccac6874083e38b33dece058
mgreen1092 <morgan.green0209@gmail.com>
2022-11-15 21:16:20 -0500

    graph styling

--- src/Components/Container/Graph/Graph.js
+++ src/Components/Container/Graph/Graph.js
@@ -12,7 +12,7 @@ import {
 function Graph (props) {
     return (
         <LineChart
-          width={500}
+          width={800}
           height={300}
           data={props.graphData}
           margin={{
