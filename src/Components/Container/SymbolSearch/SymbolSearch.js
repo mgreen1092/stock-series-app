fdcccdafe938838dedc402063019ee27402c0852
mgreen1092 <morgan.green0209@gmail.com>
2022-11-17 18:47:50 -0500

    clean code SymbolSearch

--- src/Components/Container/SymbolSearch/SymbolSearch.js
+++ src/Components/Container/SymbolSearch/SymbolSearch.js
@@ -15,7 +15,7 @@ function SymbolSearch (props) {
                     <option>Select a company:</option>
                     {data.map((tickerName) => {
                     return (
-                        <option value={tickerName.Symbol} className="Symbol-option">{tickerName.Description} | {tickerName.Symbol}</option>
+                        <option value={tickerName.Symbol}>{tickerName.Description} | {tickerName.Symbol}</option>
                     )
                     })}
                 </select>
