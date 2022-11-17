60617e2f0e107dd0610d265953a2385a2e0518c2
mgreen1092 <morgan.green0209@gmail.com>
2022-11-17 18:49:35 -0500

    clean code on Articles

--- src/Components/Container/Articles/Articles.js
+++ src/Components/Container/Articles/Articles.js
@@ -4,20 +4,20 @@ import './Articles.css'
 function Articles (props) {
     return (
         <div>
-            <h2 className='News-feed'>News Feed:</h2>
-                <div className='Article-section'>
+            <h2 className="News-feed">News Feed:</h2>
+                <div className="Article-section">
                     {props.articles?.map((article) => {
                     return (
-                        <div className='Container'>
-                             <div className='Article-content'>
-                                <div className='Article-title'>
+                        <div className="Container">
+                             <div className="Article-content">
+                                <div className="Article-title">
                                     <p><a href={article.article_url}>{article.title}</a></p>
                                 </div>
-                                <div className='Article-date'>
-                                    <p><span id='Font'>Date:</span> {article.published_utc.substring(0, 10)}</p>
+                                <div className="Article-date">
+                                    <p><span id="Font">Date:</span> {article.published_utc.substring(0, 10)}</p>
                                 </div>
-                                <div className='Article-summary'>
-                                    <p><a href={article.article_url}><span id='Font'>Summary:</span> {article.description}</a></p>
+                                <div className="Article-summary">
+                                    <p><a href={article.article_url}><span id="Font">Summary:</span> {article.description}</a></p>
                                 </div>
                             </div>
                         </div>
