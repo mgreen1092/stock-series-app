506554adaba3ba293e19f18e6f5313116e442d7d
mgreen1092 <morgan.green0209@gmail.com>
2022-11-17 18:41:38 -0500

    clean code on Description

--- src/Components/Container/Description/Description.js
+++ src/Components/Container/Description/Description.js
@@ -3,9 +3,9 @@ import '../KeyData/KeyData.css'
 
 function Description ({keyData}) {
     return (
-        <div className='KeyData-description'>
-            <h2 className='KeyData-title'><span className='ID'>Company Description:</span></h2>
-            <p className='Description-text'>{keyData.Description}</p>
+        <div className="KeyData-description">
+            <h2 className="KeyData-title"><span className="ID">Company Description:</span></h2>
+            <p className="Description-text">{keyData.Description}</p>
         </div>
     )
 }
