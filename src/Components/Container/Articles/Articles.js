20c6d4136c745894fb6a55899135d8e11e4fd63f
mgreen1092 <morgan.green0209@gmail.com>
2022-11-17 18:52:17 -0500

    clean Article file

--- src/Components/Container/Articles/Articles.js
+++ src/Components/Container/Articles/Articles.js
@@ -10,13 +10,13 @@ function Articles (props) {
                     return (
                         <div className="Container">
                              <div className="Article-content">
-                                <div className="Article-title">
+                                <div>
                                     <p><a href={article.article_url}>{article.title}</a></p>
                                 </div>
-                                <div className="Article-date">
+                                <div>
                                     <p><span id="Font">Date:</span> {article.published_utc.substring(0, 10)}</p>
                                 </div>
-                                <div className="Article-summary">
+                                <div>
                                     <p><a href={article.article_url}><span id="Font">Summary:</span> {article.description}</a></p>
                                 </div>
                             </div>
