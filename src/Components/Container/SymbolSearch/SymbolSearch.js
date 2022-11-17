50bd2f18cf08db8142c08a1c90a67232505e899a
mgreen1092 <morgan.green0209@gmail.com>
2022-11-17 09:26:13 -0500

    fixed dropdown bug

--- src/Components/Container/SymbolSearch/SymbolSearch.js
+++ src/Components/Container/SymbolSearch/SymbolSearch.js
@@ -1,25 +1,23 @@
 import React from "react";
 import './SymbolSearch.css'
 import data from '../data'
+import tickerName from "../data";
 
 function SymbolSearch (props) {
     const handleChange = (e) => {
         console.log(e.target.value)
         props.getKeyData(e.target.value)
-        //props.getValues(e.target.value)
         props.getNewsArticles(e.target.value)
         props.graph(e.target.value)
         }
+        console.log(data)
     return (
         <div className='Symbol-search'>
             <form className='Symbol-form2'>
                 <select onChange={handleChange} className='Symbol-drop'>
                     {data.map((tickerName) => {
                     return (
-                    <div>
-                        <option></option>
                         <option value={tickerName.Symbol} className='Symbol-option'>{tickerName.Description} | {tickerName.Symbol}</option>
-                    </div>
                     )
                     })}
                 </select>
