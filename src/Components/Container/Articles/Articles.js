36725da59301a2a7dbddc0401747b52af249a4d2
mgreen1092 <morgan.green0209@gmail.com>
2022-11-18 09:41:32 -0500

    css names on article

--- src/Components/Container/Articles/Articles.js
+++ src/Components/Container/Articles/Articles.js
@@ -8,13 +8,13 @@ function Articles (props) {
                 <div className="Article-section">
                     {props.articles?.map((article) => {
                     return (
-                        <div className="Container">
+                        <div className="Container-div">
                              <div className="Article-content">
                                 <div>
                                     <p><a href={article.article_url}>{article.title}</a></p>
                                 </div>
                                 <div>
-                                    <p><span id="Font">Date:</span> {article.published_utc.substring(0, 10)}</p>
+                                    <p><span id="Font-id">Date:</span> {article.published_utc.substring(0, 10)}</p>
                                 </div>
                                 <div>
                                     <p><a href={article.article_url}><span id="Font">Summary:</span> {article.description}</a></p>
