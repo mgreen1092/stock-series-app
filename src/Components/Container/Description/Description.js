b5a6fb45755093e2a0d2471de027b25ae66c2004
mgreen1092 <morgan.green0209@gmail.com>
2022-11-18 09:44:15 -0500

    css names keydata

--- src/Components/Container/Description/Description.js
+++ src/Components/Container/Description/Description.js
@@ -4,7 +4,7 @@ import '../KeyData/KeyData.css'
 function Description ({keyData}) {
     return (
         <div className="KeyData-description">
-            <h2 className="KeyData-title"><span className="ID">Company Description:</span></h2>
+            <h2 className="KeyData-title">Company Description:</h2>
             <p className="Description-text">{keyData.Description}</p>
         </div>
     )
--- src/Components/Container/KeyData/KeyData.js
+++ src/Components/Container/KeyData/KeyData.js
@@ -5,21 +5,21 @@ function KeyData ({keyData}) {
     return (
         <div className="KeyData-section">
             <div className="KeyData-title">
-                <h2><span className="ID"></span> {keyData.Name}</h2>
+                <h2><span className="ID-key"></span> {keyData.Name}</h2>
             </div>
             <div id="Key-data">
                 <div className="Sector-industry">
-                    <p><span className="ID">Exchange:</span> {keyData.Exchange}</p>
-                    <p><span className="ID">Sector:</span> {keyData.Sector}</p>
-                    <p><span className="ID">Industry:</span> {keyData.Industry}</p>
+                    <p><span className="ID-key">Exchange:</span> {keyData.Exchange}</p>
+                    <p><span className="ID-key">Sector:</span> {keyData.Sector}</p>
+                    <p><span className="ID-key">Industry:</span> {keyData.Industry}</p>
                 </div>
                 <div className="Price-Volume">
-                    <p><span className="ID">High:</span> ${keyData.high}</p>
-                    <p><span className="ID">Low:</span> ${keyData.low}</p>
-                    <p><span className="ID">Volume:</span> {keyData.volume}</p>
-                    <p><span className="ID">52 Week High:</span> ${keyData['52WeekHigh']}</p>
-                    <p><span className="ID">52 Week Low:</span> ${keyData['52WeekLow']}</p>
-                    <p><span className="ID">Earnings per share:</span> ${keyData.EPS}</p>
+                    <p><span className="ID-key">High:</span> ${keyData.high}</p>
+                    <p><span className="ID-key">Low:</span> ${keyData.low}</p>
+                    <p><span className="ID-key">Volume:</span> {keyData.volume}</p>
+                    <p><span className="ID-key">52 Week High:</span> ${keyData['52WeekHigh']}</p>
+                    <p><span className="ID-key">52 Week Low:</span> ${keyData['52WeekLow']}</p>
+                    <p><span className="ID-key">Earnings per share:</span> ${keyData.EPS}</p>
                 </div>
             </div>
         </div>
